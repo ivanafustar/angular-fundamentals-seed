4e864d47bb83dea8a81f27dcfbb9fe81f027b8ad
ivana <ivanafustar@hotmail.com>
2020-04-26 19:06:49 +0200

    - created select element and binding any existing models - using ngFor to iterate over static array - value in the <option> is set via key - using the selective property binding

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: 'passenger-form',
@@ -10,6 +11,21 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'no baggage'
+  }, {
+    key: 'hand-only',
+    value: 'hand baggage'
+  }, {
+    key: 'hold-only',
+    value: 'hold baggage'
+  }, {
+    key: 'hold-hand',
+    value: 'hand and hold baggage'
+  }]
+
 
   toggleCheckIn = (checkedIn: boolean) => {
     if (checkedIn) {
